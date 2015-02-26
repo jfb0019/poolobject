ececa9d36efaca89ff63f78fd7c1e222c2150ba8
jfb0019 <jfb0019@alu.ubu.esalumno@10.0.2.15>
2015-02-26 19:21:02 +0100

    Añadido caso de prueba para el método getInstance()

--- src/ubu/gii/dass/test/c01/ReuseblePoolTest.java
+++ src/ubu/gii/dass/test/c01/ReuseblePoolTest.java
@@ -6,20 +6,25 @@ package ubu.gii.dass.test.c01;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import ubu.gii.dass.c01.ReusablePool;
 
 /**
  * @author alumno
  *
  */
 public class ReuseblePoolTest {
+	ReusablePool reusablePool;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		reusablePool = ReusablePool.getInstance();
 	}
 
 	/**
@@ -27,14 +32,29 @@ public class ReuseblePoolTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		reusablePool = null;
 	}
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
+	 * Casos de prueba para el método getInstance de ReusablePool
 	 */
 	@Test
 	public void testGetInstance() {
-		fail("Not yet implemented");
+
+		Assert.assertNotNull(
+				"El reusablePool no debe de ser nulo. Entrada: reusablePool. Salida esperada: True.",
+				reusablePool);
+
+		Assert.assertTrue(
+				"El reusablePool tiene que ser de tipo ReusablePool. Entrada: reusablePool. Salida esperada: True.",
+				reusablePool.getClass().equals(ReusablePool.class));
+
+		ReusablePool reusablePool2 = ReusablePool.getInstance();
+		Assert.assertEquals(
+				"La instancia de reusablePool debe ser unica. Entrada: reusablePool y reusablePool2. Salida esperada: True.",
+				reusablePool, reusablePool2);
+
 	}
 
 	/**
@@ -46,7 +66,9 @@ public class ReuseblePoolTest {
 	}
 
 	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 * Test method for
+	 * {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}
+	 * .
 	 */
 	@Test
 	public void testReleaseReusable() {
